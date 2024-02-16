6001a50b37f99c22822074ba6b0fab8c4c726824
Andrii <andreyandriyenko@ukr.net>
2024-02-15 20:51:05 -0500

    test_ebay_navigation.py fixed.

--- tests/test_ebay_navigation.py
+++ tests/test_ebay_navigation.py
@@ -4,4 +4,4 @@ import pytest
 @pytest.mark.smoketest
 def test_ebay_navigation(browser):
     browser.get("http://www.ebay.com")
-    assert browser.title == "'Electronics, Cars, Fashion, Collectibles & More | eBay'"
+    assert browser.title == "Electronics, Cars, Fashion, Collectibles & More | eBay"
